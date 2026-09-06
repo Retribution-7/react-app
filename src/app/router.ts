79f55d570ffe3d3bf1cd760154bcee517166f989
Anton <ant.radkov@gmail.com>
2026-09-06 09:04:32 +0300

    feat: configure Tailwind CSS and application routing

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,9 +1,44 @@
 import { createBrowserRouter } from 'react-router';
+import { AboutCompanyPage } from '@/pages/AboutCompanyPage';
+import { AccountPage } from '@/pages/AccountPage';
+import { AdminPage } from '@/pages/AdminPage';
+import { AuthPage } from '@/pages/AuthPage';
+import { CartPage } from '@/pages/CartPage';
+import { FavoritesPage } from '@/pages/FavoritesPage';
 import { HomePage } from '@/pages/HomePage';
+import { ReviewsPage } from '@/pages/ReviewsPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     Component: HomePage,
+  },
+  {
+    path: '/cart',
+    Component: CartPage,
+  },
+  {
+    path: '/auth',
+    Component: AuthPage,
+  },
+  {
+    path: '/reviews',
+    Component: ReviewsPage,
+  },
+  {
+    path: '/about',
+    Component: AboutCompanyPage,
+  },
+  {
+    path: '/favorites',
+    Component: FavoritesPage,
+  },
+  {
+    path: '/account',
+    Component: AccountPage,
+  },
+  {
+    path: '/admin',
+    Component: AdminPage,
   },
 ]);
